a4a3b93f652c34cd6aa70e1e34dcf7211bb4c00c
Erick Lopez <erick83@gmail.com>
2021-02-12 20:00:05 -0400

    Added Opportunities search

--- routes/api.js
+++ routes/api.js
@@ -2,19 +2,47 @@ var express = require('express');
 var got = require('got');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/bios/:user', async function(req, res, next) {
   const { params } = req
-  console.log('Params', params)
   try {
     const response = await got.get(`https://torre.bio/api/bios/${params.user}`);
-    console.log(response)
     res.json(response.body);
   } catch (error) {
-    var errorNumber = error.number || parseInt(error.message.match(/\d{3}/g))
-    console.log(errorNumber)
+    var errorNumber = getErrorCode(error)
     res.status(errorNumber).json({ message: error.message })
   }
 });
+
+router.get('/opportunities/:id', async function(req, res, next) {
+  const { params } = req;
+  try {
+    const response = await got.get(`https://torre.co/api/opportunities/${params.id}`);
+    res.json(response.body);
+  } catch (error) {
+    var errorNumber = getErrorCode(error);
+    res.status(errorNumber).json({ message: error.message });
+  }
+});
+
+router.post('/opportunities', async function(req, res, next) {
+  const { query, body } = req;
+  const requestParams = {
+    searchParams: query,
+    json: body,
+    responseType: 'json'
+  }
+
+  try {
+    const response = await got.post('https://search.torre.co/opportunities/_search/', requestParams)
+    res.json(response.body);
+  } catch (error) {
+    var errorNumber = getErrorCode(error)
+    res.status(errorNumber).json({ message: error.message })
+  }
+})
+
+function getErrorCode(error) {
+  return error.number || parseInt(error.message.match(/\d{3}/g))
+}
 
 module.exports = router;
